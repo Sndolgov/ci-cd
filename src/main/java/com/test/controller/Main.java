eef7476ad14d423128fdd01d1dfb70166a4fe908
sndolgov <sndolgov84@gmail.com>
2020-12-17 18:11:09 +0200

    created

---
+++ src/main/java/com/test/controller/Main.java
@@ -0,0 +1,13 @@
+package main.java.com.test.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Main.class);
+    }
+}
